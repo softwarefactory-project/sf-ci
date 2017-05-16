3b8be47c790b91502ca7e75999a325a02060bb18
Fabien Boucher <fboucher@redhat.com>
2017-05-16 14:27:37 +0000

    WIP: First sf-ci content
    Change-Id: I671ff952729facda010ae93b0ecfbbc6448bfd9d

---
+++ run_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -ex
+
+# On slave node a Jenkins user is already created with login shell
+# To avoid further with sf-jenkins setup task issue reset it to /sbin/nologin
+sed -i '/^jenkins/ s#/sbin/sh#/sbin/nologin#' /etc/passwd
+
+USER=${SUDO_USER:-$USER}
+
+# Prepare artifacts directory
+WORKSPACE=${WORKSPACE:-/root}
+mkdir -p ${WORKSPACE}/artifacts
+
+function terminate {
+    # Restore a login shell to let us connect to the slave for debug
+    sed -i '/^jenkins/ s#/sbin/nologin#/bin/bash#' /etc/passwd
+    ansible-playbook /var/lib/software-factory/ansible/get_logs.yml 
+    # Prepare artifacts for zuul_swift_upload
+    rsync -a --no-links /root/sf-logs/ ${WORKSPACE}/artifacts
+    chown -R ${USER}:${USER} ${WORKSPACE}/artifacts
+}
+
+trap 'terminate' ERR
+
+ansible-playbook sf-init.yaml
+ansible-playbook /var/lib/software-factory/ansible/sf_install.yml
+ansible-playbook /var/lib/software-factory/ansible/sf_setup.yml
+ansible-playbook sf-serverspec.yaml
+# Bellow call will be refactored when health-check will be migrated in the sf-ci repo
+ansible-playbook sf-health-check.yaml
+ansible-playbook /tmp/health-check/zuul.yaml
+
+terminate
