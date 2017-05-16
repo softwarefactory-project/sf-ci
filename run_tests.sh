4d3405879689a31fb542b3fa980c7d4fdc81ce1e
Fabien Boucher <fboucher@redhat.com>
2017-05-16 14:27:37 +0000

    WIP: First sf-ci content
    Change-Id: I671ff952729facda010ae93b0ecfbbc6448bfd9d

---
+++ run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+# On slave node a Jenkins user is already created with login shell
+# To avoid further with sf-jenkins setup task issue reset it to /sbin/nologin
+sudo sed -i '/^jenkins/ s#/sbin/sh#/sbin/nologin#' /etc/passwd
+
+set_jenkins_shell() {
+    # Restore a login shell to let us connect to the slave for debug
+    sudo sed -i '/^jenkins/ s#/sbin/nologin#/bin/bash#' /etc/passwd
+}
+
+trap 'set_jenkins_shell' ERR
+
+sudo ansible-playbook sf-init.yaml
+sudo ansible-playbook sf-install.yaml
+sudo ansible-playbook sf-setup.yaml
+sudo ansible-playbook sf-serverspec.yaml
