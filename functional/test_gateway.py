enovance/software-factory

Add functional tests to validate job log are indexed in EL
This test pushs a change in the config repo and verify that
we are able to find logs related to the config-update last job.

Change-Id: I3df189fa65336aa977e49cd44f2e6a82459b8800

tests/functional/test_gateway.py
@@ -104,7 +104,7 @@ class TestGateway(Base):
         """ Test if all service links are shown in topmenu
         """
         subpaths = ["/r/", "/jenkins/",
-                    "/zuul/", "/etherpad/", "/paste/", "/docs/"]
+                    "/zuul/", "/etherpad/", "/paste/", "/docs/", "/app/kibana"]
         if has_issue_tracker():
             tracker = get_issue_tracker_utils(
                 auth_cookie=config.USERS[config.ADMIN_USER]['auth_cookie'])
@@ -201,6 +201,18 @@ class TestGateway(Base):
 
         # User should be known in Jenkins if logged in with SSO
         self.assertTrue(config.USER_1 in resp.text)
+
+    def test_kibana_accessible(self):
+        """ Test if Jenkins is accessible on gateway host
+        """
+        url = config.GATEWAY_URL + "/app/kibana"
+
+        # Without SSO cookie. Note that auth is no longer enforced
+
+        resp = requests.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('<title>Kibana</title>' in resp.text)
+
 
     def test_zuul_accessible(self):
         """ Test if Zuul is accessible on gateway host

tests/functional/test_log_indexed.py
@@ -1,0 +1,146 @@
+#!/bin/env python
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import shlex
+import config
+import random
+import string
+import json
+import datetime
+import time
+
+from utils import Base
+from utils import set_private_key
+from utils import GerritGitUtils
+from subprocess import Popen, PIPE
+
+
+class TestProjectReplication(Base):
+    """ Functional tests to verify the gerrit replication feature
+    """
+    def setUp(self):
+        self.un = config.ADMIN_USER
+        self.priv_key_path = set_private_key(config.USERS[self.un]["privkey"])
+        self.gitu_admin = GerritGitUtils(self.un,
+                                         self.priv_key_path,
+                                         config.USERS[self.un]['email'])
+
+    def run_ssh_cmd(self, sshkey_priv_path, user, host, subcmd):
+        host = '%s@%s' % (user, host)
+        sshcmd = ['ssh', '-o', 'LogLevel=ERROR',
+                  '-o', 'StrictHostKeyChecking=no',
+                  '-o', 'UserKnownHostsFile=/dev/null', '-i',
+                  sshkey_priv_path, host]
+        cmd = sshcmd + subcmd
+
+        p = Popen(cmd, stdout=PIPE)
+        return p.communicate(), p.returncode
+
+    def push_request_script(self, index, newhash):
+        newhash = newhash.rstrip()
+        content = """
+curl -s -XPOST 'http://elasticsearch.%s:9200/%s/_search?pretty&size=1' -d '{
+      "query": {
+          "bool": {
+              "must": [
+                  { "match": { "build_name": "config-update" } },
+                  { "match": { "build_newrev": "%s" } }
+              ]
+          }
+      }
+}'
+"""
+        with open('/tmp/test_request.sh', 'w') as fd:
+            fd.write(content % (config.GATEWAY_HOST, index, newhash))
+        cmd = ['scp', '/tmp/test_request.sh',
+               'root@%s:/tmp/test_request.sh' % config.GATEWAY_HOST]
+        p = Popen(cmd, stdout=PIPE)
+        return p.communicate(), p.returncode
+
+    def find_index(self):
+        subcmd = "curl -s -XGET http://elasticsearch.%s:9200/_cat/indices" % (
+            config.GATEWAY_HOST)
+        subcmd = shlex.split(subcmd)
+        out = self.run_ssh_cmd(config.SERVICE_PRIV_KEY_PATH, 'root',
+                               config.GATEWAY_HOST, subcmd)
+        outlines = out[0][0].split('\n')
+        outlines.pop()
+        # A logstash index is created by day
+        today_str = datetime.datetime.utcnow().strftime('%Y.%m.%d')
+        # Here we fetch the index name, but also we wait for
+        # it to appears in ElasticSearch for 5 mins
+        retries = 300
+        index = []
+        while True:
+            try:
+                retries -= 1
+                index = [o for o in outlines if
+                         o.split()[2].startswith('logstash-%s' % today_str)]
+                break
+            except IndexError:
+                if retries > 0:
+                    time.sleep(1)
+                    continue
+        self.assertEqual(
+            len(index),
+            1,
+            "No logstash index has been found for today logstash-%s" % (
+                today_str))
+        index = index[0].split()[2]
+        return index
+
+    def verify_logs_exported(self):
+        subcmd = "bash /tmp/test_request.sh"
+        subcmd = shlex.split(subcmd)
+        retries = 300
+        while True:
+            retries -= 1
+            out = self.run_ssh_cmd(config.SERVICE_PRIV_KEY_PATH, 'root',
+                                   config.GATEWAY_HOST, subcmd)
+            ret = json.loads(out[0][0])
+            if len(ret['hits']['hits']) >= 1:
+                break
+            elif len(ret['hits']['hits']) == 0:
+                if retries > 0:
+                    time.sleep(1)
+                    continue
+        self.assertEqual(len(ret['hits']['hits']),
+                         1,
+                         "Fail to find our log in ElasticSeach")
+        return ret['hits']['hits'][0]
+
+    def direct_push_in_config_repo(self, url, pname='config'):
+        rand_str = ''.join(random.choice(
+            string.ascii_uppercase + string.digits) for _ in range(5))
+        clone = self.gitu_admin.clone(url, pname)
+        with open('%s/test_%s' % (clone, rand_str), 'w') as fd:
+            fd.write('test')
+        self.gitu_admin.add_commit_in_branch(
+            clone, 'master', ['test_%s' % rand_str])
+        head = file('%s/.git/refs/heads/master' % clone).read()
+        self.gitu_admin.direct_push_branch(clone, 'master')
+        return head
+
+    def test_log_indexation(self):
+        """ Test job log are exported in Elasticsearch
+        """
+        head = self.direct_push_in_config_repo(
+            'ssh://admin@%s:29418/config' % (
+                config.GATEWAY_HOST))
+        index = self.find_index()
+        self.push_request_script(index, head)
+        log = self.verify_logs_exported()
+        self.assertEqual(log['_source']["build_name"], "config-update")

softwarefactory-project/sf-ci

functional/test_gateway.py
@@ -104,7 +104,7 @@ class TestGateway(Base):
         """ Test if all service links are shown in topmenu
         """
         subpaths = ["/r/", "/jenkins/",
-                    "/zuul/", "/etherpad/", "/paste/", "/docs/"]
+                    "/zuul/", "/etherpad/", "/paste/", "/docs/", "/app/kibana"]
         if has_issue_tracker():
             tracker = get_issue_tracker_utils(
                 auth_cookie=config.USERS[config.ADMIN_USER]['auth_cookie'])
@@ -201,6 +201,17 @@ class TestGateway(Base):
 
         # User should be known in Jenkins if logged in with SSO
         self.assertTrue(config.USER_1 in resp.text)
+
+    def test_kibana_accessible(self):
+        """ Test if Kibana is accessible on gateway host
+        """
+        url = config.GATEWAY_URL + "/app/kibana"
+
+        # Without SSO cookie. Note that auth is no longer enforced
+
+        resp = requests.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('<title>Kibana</title>' in resp.text)
 
     def test_zuul_accessible(self):
         """ Test if Zuul is accessible on gateway host

functional/test_log_indexed.py
@@ -1,0 +1,149 @@
+#!/bin/env python
+#
+# Copyright (2016) Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import shlex
+import config
+import random
+import string
+import json
+import datetime
+import time
+
+from utils import Base
+from utils import set_private_key
+from utils import GerritGitUtils
+from subprocess import Popen, PIPE
+
+
+class TestLogExportedInElasticSearch(Base):
+    """ Functional tests to verify job logs are exported in ElasticSearch
+    """
+    def setUp(self):
+        self.un = config.ADMIN_USER
+        self.priv_key_path = set_private_key(config.USERS[self.un]["privkey"])
+        self.gitu_admin = GerritGitUtils(self.un,
+                                         self.priv_key_path,
+                                         config.USERS[self.un]['email'])
+
+    def run_ssh_cmd(self, sshkey_priv_path, user, host, subcmd):
+        host = '%s@%s' % (user, host)
+        sshcmd = ['ssh', '-o', 'LogLevel=ERROR',
+                  '-o', 'StrictHostKeyChecking=no',
+                  '-o', 'UserKnownHostsFile=/dev/null', '-i',
+                  sshkey_priv_path, host]
+        cmd = sshcmd + subcmd
+
+        p = Popen(cmd, stdout=PIPE)
+        return p.communicate(), p.returncode
+
+    def push_request_script(self, index, newhash):
+        newhash = newhash.rstrip()
+        content = """
+curl -s -XPOST 'http://elasticsearch.%s:9200/%s/_search?pretty&size=1' -d '{
+      "query": {
+          "bool": {
+              "must": [
+                  { "match": { "build_name": "config-update" } },
+                  { "match": { "build_newrev": "%s" } }
+              ]
+          }
+      }
+}'
+"""
+        with open('/tmp/test_request.sh', 'w') as fd:
+            fd.write(content % (config.GATEWAY_HOST, index, newhash))
+        cmd = ['scp', '/tmp/test_request.sh',
+               'root@%s:/tmp/test_request.sh' % config.GATEWAY_HOST]
+        p = Popen(cmd, stdout=PIPE)
+        return p.communicate(), p.returncode
+
+    def find_index(self):
+        subcmd = "curl -s -XGET http://elasticsearch.%s:9200/_cat/indices" % (
+            config.GATEWAY_HOST)
+        subcmd = shlex.split(subcmd)
+        # A logstash index is created by day
+        today_str = datetime.datetime.utcnow().strftime('%Y.%m.%d')
+        # Here we fetch the index name, but also we wait for
+        # it to appears in ElasticSearch for 5 mins
+        retries = 300
+        index = []
+        while True:
+            try:
+                out = self.run_ssh_cmd(config.SERVICE_PRIV_KEY_PATH, 'root',
+                                       config.GATEWAY_HOST, subcmd)
+                outlines = out[0][0].split('\n')
+                outlines.pop()
+                retries -= 1
+                index = [o for o in outlines if
+                         o.split()[2].startswith('logstash-%s' % today_str)]
+                if not len(index):
+                    raise Exception()
+                else:
+                    break
+            except:
+                if retries > 0:
+                    time.sleep(1)
+                    continue
+        self.assertEqual(
+            len(index),
+            1,
+            "No logstash index has been found for today logstash-%s (%s)" % (
+                today_str, str(index)))
+        index = index[0].split()[2]
+        return index
+
+    def verify_logs_exported(self):
+        subcmd = "bash /tmp/test_request.sh"
+        subcmd = shlex.split(subcmd)
+        retries = 300
+        while True:
+            retries -= 1
+            out = self.run_ssh_cmd(config.SERVICE_PRIV_KEY_PATH, 'root',
+                                   config.GATEWAY_HOST, subcmd)
+            ret = json.loads(out[0][0])
+            if len(ret['hits']['hits']) >= 1:
+                break
+            elif len(ret['hits']['hits']) == 0:
+                if retries > 0:
+                    time.sleep(1)
+                    continue
+        self.assertEqual(len(ret['hits']['hits']),
+                         1,
+                         "Fail to find our log in ElasticSeach")
+        return ret['hits']['hits'][0]
+
+    def direct_push_in_config_repo(self, url, pname='config'):
+        rand_str = ''.join(random.choice(
+            string.ascii_uppercase + string.digits) for _ in range(5))
+        clone = self.gitu_admin.clone(url, pname)
+        with open('%s/test_%s' % (clone, rand_str), 'w') as fd:
+            fd.write('test')
+        self.gitu_admin.add_commit_in_branch(
+            clone, 'master', ['test_%s' % rand_str])
+        head = file('%s/.git/refs/heads/master' % clone).read()
+        self.gitu_admin.direct_push_branch(clone, 'master')
+        return head
+
+    def test_log_indexation(self):
+        """ Test job log are exported in Elasticsearch
+        """
+        head = self.direct_push_in_config_repo(
+            'ssh://admin@%s:29418/config' % (
+                config.GATEWAY_HOST))
+        index = self.find_index()
+        self.push_request_script(index, head)
+        log = self.verify_logs_exported()
+        self.assertEqual(log['_source']["build_name"], "config-update")