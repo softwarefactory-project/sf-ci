enovance/software-factory

SF project namespace check get project details
Verify project details endpoint on managesf behave
as expected with a project including a '/' in its name.
This endpoint is used by the dashboard

Change-Id: I9d6064ee74ca2a34f7b5676ead383676180f54c0

tests/functional/test_managesf.py
@@ -568,6 +568,14 @@ class TestManageSF(Base):
         self.assertTrue('config' in resp.json())
         resp = requests.get(url, cookies=user2_cookies)
 
+        # Validate the same behavior with project including a '/'
+        project = 'p/%s' % create_random_str()
+        self.create_project(project, config.USER_2)
+        url = "http://%s%s" % (config.GATEWAY_HOST, "/manage/project/")
+        resp = requests.get(url, cookies=admin_cookies)
+        self.assertEqual(200, resp.status_code)
+        self.assertTrue(project in resp.json())
+
     def test_project_pages_config(self):
         """ Check if managesf allow us to configure pages for a project
         """

softwarefactory-project/sf-ci

functional/test_managesf.py
@@ -567,6 +567,17 @@ class TestManageSF(Base):
         self.assertTrue('config' in resp.json())
         resp = requests.get(url, cookies=user2_cookies)
 
+        # Validate the same behavior with project including a '/'
+        project = 'p/%s' % create_random_str()
+        self.create_project(project, config.USER_2)
+        url = "http://%s%s" % (config.GATEWAY_HOST, "/manage/project/")
+        # Wait 15 seconds for managesf cache invalidation
+        import time
+        time.sleep(15)
+        resp = requests.get(url, cookies=user2_cookies)
+        self.assertEqual(200, resp.status_code)
+        self.assertTrue(project in resp.json())
+
     def test_project_pages_config(self):
         """ Check if managesf allow us to configure pages for a project
         """