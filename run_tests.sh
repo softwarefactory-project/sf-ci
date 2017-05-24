9013e7804dbefaf3496722dca9a57f0bc28e914e
Tristan Cacqueray <tdecacqu@redhat.com>
2017-05-24 10:50:59 +0000

    Add sf-upgrade
    Change-Id: I716448d9a1e37896d0ff963c770c300737b2550b
    Depends-On: I6973e2c87641079be523f33c348cff1b837a920d

--- run_tests.sh
+++ run_tests.sh
@@ -2,13 +2,8 @@
 
 set -Ex
 
-test -f ~/.local/bin/ara 2> /dev/null || {
-    sudo yum install -y python-pip
-    pip install --user 'ara==0.12.4'
-}
-test -d /usr/lib/python2.7/site-packages/nose_htmloutput || {
-    pip install nose-htmloutput
-}
+TEST_TYPE="$1"
+ARCH="$2"
 
 rm -Rf ~/.ara/
 export ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
@@ -29,14 +24,16 @@ mkdir -p ${ARTIFACTS}
 
 function terminate {
     # Restore a login shell to let us connect to the slave for debug
-    sed -i '/^jenkins/ s#/sbin/nologin#/bin/bash#' /etc/passwd
+    sed -i '/^jenkins/ s#:[^:]*$#:/bin/bash#' /etc/passwd
     ansible-playbook /var/lib/software-factory/ansible/get_logs.yml
     # Prepare artifacts for zuul_swift_upload
     rsync -a --no-links /root/sf-logs/ ${ARTIFACTS}/
     pushd ${ARTIFACTS}
         rm -Rf html
-        ~/.local/bin/ara generate html
+        ara generate html
     popd
+    cp -Rv /etc/yum.repos.d/ ${ARTIFACTS}
+    rsync -anvi /etc/yum.repos.d/ ${ARTIFACTS}/yum.repos.d/ --exclude "CentOS-*.repo"
     tar -czf ${ARTIFACTS}.tgz ${ARTIFACTS}
     mv ${ARTIFACTS}.tgz ${ARTIFACTS}/
     chown -R ${USER}:${USER} ${ARTIFACTS}
@@ -52,11 +49,40 @@ function run_functional_tests() {
     mv nose_results.html ${ARTIFACTS}/
 }
 
-ansible-playbook sf-init.yaml
+# TODO: make this a parameter
+if [ "${TEST_TYPE}" == "upgrade" ]; then
+    VERSION="2.5.0"
+
+    ansible-playbook -e "sf_arch=${ARCH} sf_version=${VERSION}" sf-init-stable.yaml
+else
+    ansible-playbook -e "sf_arch=${ARCH}" sf-init-master.yaml
+fi
+
+
+# Deploy
 ansible-playbook /var/lib/software-factory/ansible/sf_install.yml
 ansible-playbook /var/lib/software-factory/ansible/sf_setup.yml
+
+# TODO: run provisioner
+
+if [ "${TEST_TYPE}" == "upgrade" ]; then
+    ansible-playbook sf-upgrade.yaml
+    ansible-playbook /var/lib/software-factory/ansible/sf_upgrade.yml
+    ansible-playbook /var/lib/software-factory/ansible/sf_install.yml
+    ansible-playbook /var/lib/software-factory/ansible/sf_setup.yml
+
+    rpm -qa | sort > package_upgraded
+    diff /var/lib/software-factory/package_installed package_upgraded || true
+    # TODO: run provisioner check
+fi
+
 ansible-playbook sf-serverspec.yaml
 ansible-playbook health-check/sf-health-check.yaml
 run_functional_tests
+
+if [ "${TEST_TYPE}" == "functional" ]; then
+    # TODO: erase deployment and recover backup test
+    echo pass
+fi
 
 terminate 'END'
