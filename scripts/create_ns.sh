bee2e17dbb234b6c4bae10db8b98af9c792a7cba
Tristan Cacqueray <tdecacqu@redhat.com>
2017-06-01 02:27:09 +0000

    Refactor sf-ci into 2 main playbooks
    Change-Id: I0d40f3c94ab8e80522ec06e633855cb6fe385ec8

---
+++ scripts/create_ns.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Simple netns creation
+
+if [ -z "$SUDO_COMMAND" ]; then
+    echo "$0: run with sudo"
+    exit 1
+fi
+
+if ! ip netns | grep local-test -q; then
+    ip netns add local-test
+    ip link add vlocal0 type veth peer name vlocal1
+    ip link set vlocal0 up
+    ip addr add 192.168.42.1/30 dev vlocal0
+
+    ip link set vlocal1 netns local-test up
+    ip netns exec local-test ip addr add 192.168.42.2/30 dev vlocal1
+    ip netns exec local-test ip route add default via 192.168.42.1
+fi
+
+if [ "$1" == "stop" ]; then
+    ip link delete vlocal0
+        ip netns delete local-test
+    exit 0
+fi
+
+ip netns exec local-test sudo -u $SUDO_USER "$@"
