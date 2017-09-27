215a843d093d9522e850bf804d694ce1599d881b
Tristan Cacqueray <tdecacqu@redhat.com>
2017-09-27 20:37:48 +0000

    Support ZuulV3 only deployment
    * Add SF_ZUUL_EXECUTOR variable to query zuul-web instead of managesf
    
    Depends-On: I40ed5b5c18391394a2be2b84dbf4569cb1e2cd4f
    Depends-On: I37f9059f416652bcbc159c58ebcad3ee6619b61b
    Change-Id: Ib1047a132d03ef50d960ae54d52087321dd1af6c

--- tests/functional/test_resources_workflow.py
+++ tests/functional/test_resources_workflow.py
@@ -93,7 +93,8 @@ class TestResourcesWorkflow(Base):
             config_clone_dir,
             "Add new resources for functional tests")
         config_update_log = self.ju.wait_for_config_update(change_sha)
-        self.assertIn("SUCCESS", config_update_log)
+        self.assertEqual(
+            len(re.findall('managesf\..*failed=0', config_update_log)), 1)
 
     def propose_resources_change_check_ci(
             self, fpath, resources=None,
@@ -587,7 +588,8 @@ class TestResourcesWorkflow(Base):
         # If not True then we cannot concider config-update succeed
         config_update_log = self.ju.wait_for_config_update(revision)
         self.assertIn("Skip resources apply.", config_update_log)
-        self.assertIn("SUCCESS", config_update_log)
+        self.assertEqual(
+            len(re.findall('managesf\..*failed=0', config_update_log)), 1)
         # Checking again missing resources  must return nothing
         ret = requests.get("%s/manage/resources/?get_missing_"
                            "resources=true" % config.GATEWAY_URL,
