5ef6ee93e100494bd2d1a0c9724e2ba1fe0efb38
Tristan Cacqueray <tdecacqu@redhat.com>
2017-12-06 06:41:06 +0000

    Skip test that doesn't work without zuulv2
    Also improve the wait_for_config_update method to return the result.
    
    Change-Id: Icb92e426c52634fa43e38355bda4e92e2f80ef05

--- tests/functional/test_log_indexed.py
+++ tests/functional/test_log_indexed.py
@@ -126,7 +126,7 @@ curl -s -XPOST 'http://elasticsearch.%s:9200/%s/_search?pretty&size=1' -d '{
         self.gitu_admin.direct_push_branch(clone, 'master')
         return head
 
-    @skipIfServiceMissing('elasticsearch')
+    @skipIfServiceMissing('job-logs-gearman-worker')
     def test_log_indexation(self):
         """ Test job log are exported in Elasticsearch
         """
