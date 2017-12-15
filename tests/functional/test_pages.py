7202ca4ad8f3733c94ae0dd81bbde6a3dd919c14
Fabien Boucher <fboucher@redhat.com>
2017-12-15 17:42:46 +0000

    sfpages test update for zuulv3
    Change-Id: Ie825d9b0c6a0f46783d8a287419d96b83a2d9256
    Depends-on: Ie1481cd7017153007329bfa158b63432b4975b58

--- tests/functional/test_pages.py
+++ tests/functional/test_pages.py
@@ -13,7 +13,6 @@
 # under the License.
 
 import os
-import time
 import config
 import shutil
 import requests
@@ -22,8 +21,10 @@ from utils import Base
 from utils import ResourcesUtils
 from utils import set_private_key
 from utils import GerritGitUtils
+from utils import JenkinsUtils
 from utils import create_random_str
-from utils import skipIfServiceMissing
+
+from pysflib.sfgerrit import GerritUtils
 
 
 class TestPages(Base):
@@ -40,10 +41,12 @@ class TestPages(Base):
             config.ADMIN_USER,
             priv_key_path,
             config.USERS[config.ADMIN_USER]['email'])
+        self.gu = GerritUtils(
+            config.GATEWAY_URL,
+            auth_cookie=config.USERS[config.ADMIN_USER]['auth_cookie'])
+        self.ju = JenkinsUtils()
 
     def tearDown(self):
-        for name in self.projects:
-            self.ru.delete_repo(name)
         for dirs in self.dirs_to_delete:
             shutil.rmtree(dirs)
 
@@ -65,27 +68,67 @@ class TestPages(Base):
         self.gitu_admin.add_commit_for_all_new_additions(clone_dir, msg)
         return self.gitu_admin.direct_push_branch(clone_dir, 'master')
 
-    @skipIfServiceMissing('pages')
     def test_pages(self):
         """ Test sf-pages publication - raw content
         """
-        # This test verify raw content is published and accessible
-        pname = "%s.%s" % (create_random_str(), config.GATEWAY_HOST)
+        # Create project
+        pname = create_random_str()
         self.create_project(pname)
+
+        # Register it in zuul
+        config_dir = self.clone_as_admin("config")
+        tenant = os.path.join(config_dir, "zuulV3", pname + ".yaml")
+        job = os.path.join(config_dir, "zuul.d", pname + ".yaml")
+        file(tenant, 'w').write("""---
+- tenant:
+    name: 'local'
+    source:
+      gerrit:
+        untrusted-projects:
+          - %s
+""" % pname)
+        file(job, 'w').write("""---
+- job:
+    name: %(pname)s-build-and-publish-website
+    parent: build-and-publish-pages
+    vars:
+      vhost_name: %(pname)s
+    final: true
+    allowed-projects:
+      - %(pname)s
+""" % {'pname': pname})
+        change_sha = self.commit_direct_push_as_admin(
+            config_dir, "Set website job")
+        config_update_result = self.ju.wait_for_config_update(
+            change_sha, return_result=True)
+        self.assertEqual(config_update_result, 'SUCCESS')
+
+        # Add raw content and define .zuul.yaml
         clone_dir = self.clone_as_admin(pname)
         path = os.path.join(clone_dir, "index.html")
+        zuul = os.path.join(clone_dir, ".zuul.yaml")
         file(path, 'w').write("<b>Hello World !</b>")
-        self.commit_direct_push_as_admin(clone_dir, "Update website")
-        c = 0
-        while True:
-            if c >= 90:
-                raise Exception("Unable to find the publication")
-            resp = requests.get("http://%s" % config.GATEWAY_HOST,
-                                headers={'Host': pname})
-            if resp.status_code == 200:
-                content = resp.text
-                self.assertEqual(content, '<b>Hello World !</b>')
-                break
-            else:
-                time.sleep(1)
-                c += 1
+        file(zuul, 'w').write("""---
+- project:
+    name: %s
+    check:
+      jobs:
+        - %s-build-and-publish-website
+""" % (pname, pname))
+        # We run the publication in the check pipeline
+        # to speed up the test, should be done in the gate
+        # in the real life
+        change_sha = self.gitu_admin.add_commit_and_publish(
+            clone_dir, "master", "Test change",
+            fnames=['.zuul.yaml', 'index.html'])
+        change_nr = self.gu.get_change_number(change_sha)
+        note = self.gu.wait_for_verify(
+            change_nr, ['zuul'], timeout=240)
+        self.assertEqual(note, 1)
+
+        # Now attempt to access the published content
+        resp = requests.get(
+            "http://%s" % config.GATEWAY_HOST,
+            headers={'Host': "%s.%s" % (pname, config.GATEWAY_HOST)})
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.text, '<b>Hello World !</b>')
