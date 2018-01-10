e9bf3d10c2d01841d5b0606bebfab93f5949d62d
mhuin <mhuin@redhat.com>
2018-01-10 10:45:28 +0100

    make zuul3 web API calls through the manageSF proxy
    Depends-On: Ia5da000701d546e0a66dda931275a0d6bfee16f8
    
    Change-Id: I40ad36c0492d4eccb24747d6364a240475b64b58

--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -499,8 +499,10 @@ class JenkinsUtils(Tool):
         return "FAILED"
 
     def wait_for_config_update_zuul3(self, revision, return_result=False):
-        job_url = ("%s/zuul3/local/builds.json?job_name=config-update&"
-                   "newrev=%s") % (config.GATEWAY_URL, revision)
+        job_url = ("%sv2/zuul/local/builds.json?job_name=config-update&"
+                   "newrev=%s") % (
+                       config.MANAGESF_API,
+                       revision)
         logger.debug("Waiting for config-update using %s" % job_url)
         r = None
         try:
