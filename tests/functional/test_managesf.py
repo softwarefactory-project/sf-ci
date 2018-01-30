3fa52b08b74f15b71fc9e9b76a7f19d390f1accb
mhuin <mhuin@redhat.com>
2018-01-30 10:33:46 +0100

    Test that the passthrough mechanism returns correct results
    Change-Id: Idd48ac60cfb90a19d79cc07118be8c608ee3a6c2
    Depends-On: Icc028c724d57a83ffe33d8b86604c1a1351dc4ab

--- tests/functional/test_managesf.py
+++ tests/functional/test_managesf.py
@@ -76,3 +76,16 @@ class TestManageSF(Base):
         users = self.msu.exe(cmd)
         self.assertTrue(config.USER_2 in users,
                         "'%s' returned %s" % (cmd, users))
+
+    @skipIfServiceMissing('zuul3')
+    def test_apipassthrough(self):
+        """Test if REST services are accessible"""
+        direct_url = config.GATEWAY_URL + "/zuul3/local/builds.json"
+        passthrough_url = (config.GATEWAY_URL +
+                           "/manage/v2/zuul/local/builds.json")
+        direct_resp = requests.get(direct_url)
+        passthrough_rest = requests.get(passthrough_url)
+        self.assertEqual(direct_resp.status_code,
+                         passthrough_rest.status_code)
+        self.assertEqual(direct_resp.json(),
+                         passthrough_rest.json())
