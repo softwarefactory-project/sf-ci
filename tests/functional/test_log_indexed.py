ba01cf460edb5760b57ccd71eed8e45d9f1a718d
Tristan Cacqueray <tdecacqu@redhat.com>
2019-11-06 13:53:12 +0000

    Switch to python3
    Depends-On: https://softwarefactory-project.io/r/16948
    Depends-On: https://softwarefactory-project.io/r/16944
    Change-Id: I11d77e6363417f19d834d86540ced3bf2e2a4d3e

--- tests/functional/test_log_indexed.py
+++ tests/functional/test_log_indexed.py
@@ -19,7 +19,7 @@ import random
 import string
 import subprocess
 import time
-import urllib
+import urllib.request
 
 from utils import Base, skipIfServiceMissing
 from utils import set_private_key
@@ -40,7 +40,7 @@ class TestLogExportedInElasticSearch(Base):
     def copy_request_script(self, index, newhash):
         newhash = newhash.rstrip()
         elastic_url = '%s/elasticsearch' % config.GATEWAY_URL
-        data = json.loads(urllib.urlopen(elastic_url).read())
+        data = json.loads(urllib.request.urlopen(elastic_url).read())
         if data['version']['number'] == '2.4.6':
             extra_headers = " -H 'kbn-version:4.5.4'"
         else:
@@ -68,7 +68,7 @@ curl -s -XPOST '%s/%s/_search?pretty&size=1' %s -d '{
         # Here we fetch the index name, but also we wait for
         # it to appears in ElasticSearch for 5 mins
         index = []
-        for retry in xrange(300):
+        for retry in range(300):
             try:
                 outlines = subprocess.check_output(subcmd).split('\n')
                 indexes = filter(
@@ -87,7 +87,7 @@ curl -s -XPOST '%s/%s/_search?pretty&size=1' %s -d '{
 
     def verify_logs_exported(self):
         subcmd = ["bash", "/tmp/test_request.sh"]
-        for retry in xrange(300):
+        for retry in range(300):
             out = subprocess.check_output(subcmd)
             ret = json.loads(out)
             if len(ret['hits']['hits']) >= 1:
